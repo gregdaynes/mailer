1929c414b2dfce60fb228d2ef3b17d50a326ee99
Gregory Daynes <email@gregdaynes.com>
2024-12-09 20:11:13 -0800

    chore: setup method argument validation

--- examples/example.js
+++ examples/example.js
@@ -1,12 +1,32 @@
 import Path from 'node:path'
 import Fastify from "fastify"
 
-const fastify = Fastify({ logger: true })
+const fastify = Fastify({ logger: { level: 'debug' } })
 
 const opts = {
 
 }
 fastify.register(import(Path.join(import.meta.dirname, '..', 'index.js'), opts))
+
+fastify.get('/', async (request, reply) => {
+  try {
+    request.mailer({
+      nonce: 'abc123',
+      to: 'alice@example.com',
+      from: 'bob@example.com',
+      subject: 'Example notification',
+      template: 'Hi, ${name}!',
+      data: {
+        name: 'Alice'
+      },
+    })
+  } catch (err) {
+    request.log.error(err)
+    return reply.status(400).send('Error')
+  }
+
+  return reply.send('ok')
+})
 
 fastify.listen({ port: 3000 }, err => {
   if (err) throw err
--- index.js
+++ index.js
@@ -1,8 +1,65 @@
 import fp from 'fastify-plugin'
+import Ajv from 'ajv'
+
+const ajv = new Ajv({
+  removeAdditional: 'all',
+  useDefaults: true,
+  coerceTypes: 'array',
+})
+
+const schema = {
+  $id: 'mailer:create',
+  type: 'object',
+  properties: {
+    nonce: {
+      type: 'string'
+    },
+    to: {
+      type: 'string',
+      format: 'email'
+    },
+    from: {
+      type: 'string',
+      format: 'email',
+    },
+    subject: {
+      type: 'string',
+      minLength: 1,
+      maxLength: 255,
+    },
+    template: {
+      type: 'string',
+    },
+    data: {
+      type: 'object',
+    }
+  },
+  required: [
+    'nonce',
+    'to',
+    'from',
+    'subject',
+    'template',
+    'data'
+  ]
+}
 
 export default fp(function (fastify, opts) {
-  return
+  const validate = ajv.compile(schema)
+
+  fastify.decorateRequest('mailer', function (args) {
+    const valid = validate(args)
+    if (!valid) {
+      fastify.log.error({ errors: validate.errors })
+      throw new Error('Validation error', { cause: validate.errors[0] })
+    }
+
+    fastify.log.debug(args, 'arguments passed to function')
+  })
 },
 {
   name: 'notifications'
 })
+
+// api to send email
+// needs a schema
